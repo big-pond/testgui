b9d7a9b8fb68502a615385c353081d58786994c1
Yri Popov <py99@mail.ru>
2023-09-30 15:43:01 +0500

    first commit

---
+++ testtk.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#-*-coding: utf-8-*-
+
+import tkinter as tk
+import tkinter.ttk as ttk
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.grid()
+combobox = ttk.Combobox(frame, values = ["ОДИН", "ДВА", "ТРИ"], height=3)
+combobox.set(u'ОДИН')
+combobox.grid(column=0,row=0)
+root.mainloop()
---
+++ testtk1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#-*-coding: utf-8-*-
+
+from tkinter import *
+
+window = Tk()
+window.title("Добро пожаловать в приложение PythonRu")
+lbl = Label(window, text="Привет мир")
+lbl.grid(column=0, row=0)
+window.mainloop()
